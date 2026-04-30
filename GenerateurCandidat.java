f5b08c1faefd0d35f0597d0781f4743feaac0323
Youssef Kossentini <kossentiniyoussef1@gmail.com>
2026-04-30 12:52:09 +0100

    Add files via upload

---
+++ GenerateurCandidat.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public interface GenerateurCandidat {
+    public abstract void genererCandidat(ArrayList<Name> listeDeNom, ArrayList<Name> newListe);
+}
---
+++ Resultat.java
@@ -0,0 +1,8 @@
+public class Resultat {
+    private double score;
+    private Name candidat;
+
+    public double getScore() {
+        return this.score;
+    }
+}
