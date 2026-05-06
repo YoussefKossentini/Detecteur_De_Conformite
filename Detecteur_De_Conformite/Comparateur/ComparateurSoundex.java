f20e946ef926cdd9f8439bb42866689d4691021d
Youssef Kossentini <youssef.kossentini@etudiant-enit.utm.tn>
2026-05-06 20:55:37 +0100

    Update ComparateurSoundex.java

--- Detecteur_De_Conformite/Comparateur/ComparateurSoundex.java
+++ Detecteur_De_Conformite/Comparateur/ComparateurSoundex.java
@@ -10,7 +10,7 @@ public class ComparateurSoundex implements Comparateur {
         StringBuilder code = new StringBuilder();
         code.append(s.charAt(0));
 
-        String map = "01230120022455012623010202";
+        String map = "01230120022455012623010202";  //C'est le tableau de correspondance Soundex. Chaque lettre de l'alphabet A-Z est mappée à un chiffre selon son groupe phonétique 
         char prev = map.charAt(s.charAt(0) - 'A');
 
         for (int i = 1; i < s.length() && code.length() < 4; i++) {
@@ -29,7 +29,22 @@ public class ComparateurSoundex implements Comparateur {
 
         return code.toString();
     }
-
-    public boolean comparer(ArrayList<String> s1, ArrayList<String> s2) {
+// la logique ici est la suivante : pour chaque token de s1, on calcule son code Soundex et on cherche au moins un token dans s2 avec le même code.
+//Si un token de s1 n'a aucun jumeau phonétique dans s2 alors on obtient comme output false.
+    public boolean comparer(ArrayList<String> s1, ArrayList<String> s2) { 
+        if (s1 == null || s2 == null || s1.isEmpty() || s2.isEmpty())
+            return false;
+        for (String t1 : s1) {
+            boolean trouve = false;
+            for (String t2 : s2) {
+                if (soundex(t1).equals(soundex(t2))) {
+                    trouve = true;
+                     break;
+                }
+            }
+            if (!trouve) return false;
+        }
+        return true;
     }
+        }
 }
