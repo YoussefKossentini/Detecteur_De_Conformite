2211642807adf3b7492c1fc43baad427ed51db64
Youssef Kossentini <youssef.kossentini@etudiant-enit.utm.tn>
2026-05-15 09:56:42 +0100

    Update run.sh

--- Detecteur_De_Conformite/run.sh
+++ Detecteur_De_Conformite/run.sh
@@ -6,7 +6,7 @@ javac -cp ".:lib/*" **/*.java
 java -cp ".:lib/*" ui.MenuKYC 
 
 
-#Pour WINDOWS voici le code shell (juste supprimez les # des lignes de codes et les mets pour la version MACOS) et changez l'extension a .bat ( le nom du fichier lui même devient de run.sh a run.bat) 
+#Pour WINDOWS voici le code shell (juste supprimez les # des lignes de codes et les mets pour la version MACOS) et changez l'extension a .bat ( le nom du fichier lui même se renomme de run.sh a run.bat) 
 #cd $PSScriptRoot
 #javac -cp ".;lib/*" (Get-ChildItem -Recurse -Filter *.java | % { $_.FullName })
 #java -cp ".;lib/*" ui.MenuKYC
