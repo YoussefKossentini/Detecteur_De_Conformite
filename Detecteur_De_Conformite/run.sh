05cd39fe96da1205087ec8169d98bbb007ca3709
Youssef Kossentini <youssef.kossentini@etudiant-enit.utm.tn>
2026-05-15 09:18:28 +0100

    Update run.sh

--- Detecteur_De_Conformite/run.sh
+++ Detecteur_De_Conformite/run.sh
@@ -2,6 +2,6 @@
 
 cd "$(dirname "$0")"
 
-javac -cp ".:lib/*" **/*.java || { echo " Échec de la compilation. Vérifiez que icu4j.jar est dans le dossier lib/."; exit 1; }
+javac -cp ".:lib/*" **/*.java 
 
 java -cp ".:lib/*" ui.MenuKYC 
