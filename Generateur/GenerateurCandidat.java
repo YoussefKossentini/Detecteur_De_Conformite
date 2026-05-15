e8bd41e35bc16d5f220c78583aad19ac8cc42954
Koussay ltifi <koussay.ltifi@etudiant-enit.utm.tn>
2026-05-15 09:34:48 +0100

    Delete Generateur directory

--- Generateur/GenerateurCandidat.java
+++
@@ -1,7 +0,0 @@
-package generateur;
-import java.util.ArrayList;
-import model.Name;
-
-public interface GenerateurCandidat {
-    ArrayList<ArrayList<Name>> genererCandidats(ArrayList<Name> listeA, ArrayList<Name> listeB);
-}
