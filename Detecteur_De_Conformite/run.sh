a68b48617355ba77a4f3192954cee63cd3fb2fca
Youssef Kossentini <youssef.kossentini@etudiant-enit.utm.tn>
2026-05-15 09:49:41 +0100

    Update run.sh

--- Detecteur_De_Conformite/run.sh
+++ Detecteur_De_Conformite/run.sh
@@ -6,7 +6,7 @@ javac -cp ".:lib/*" **/*.java
 java -cp ".:lib/*" ui.MenuKYC 
 
 
-#Pour WINDOWS voici le code shell
+#Pour WINDOWS voici le code shell (juste supprimez les # des lignes de codes et les mets pour la version MACOS)
 #cd $PSScriptRoot
 #javac -cp ".;lib/*" (Get-ChildItem -Recurse -Filter *.java | % { $_.FullName })
 #java -cp ".;lib/*" ui.MenuKYC
